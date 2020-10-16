59d9e0a7b14df07dc4c40ca5d51d4e6bb1d7b301
Aayan <aayankhare23@gmail.com>
2020-10-15 17:57:22 -0700

    c33

--- Bird.js
+++ Bird.js
@@ -6,6 +6,7 @@ class Bird extends BaseClass {
     this.trajectory =[];
   }
 
+
   display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
@@ -16,8 +17,7 @@ class Bird extends BaseClass {
       var position = [this.body.position.x, this.body.position.y];
       this.trajectory.push(position);
     }
-   
-
+  
     for(var i=0; i<this.trajectory.length; i++){
       image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1]);
     }
